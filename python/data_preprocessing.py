2ba7fe321a94c0dce5e13dd9db340ab38564d76d
wada <115588841+wadagz@users.noreply.github.com>
2025-07-17 11:39:56 -0600

    style: reestructurado y comentado data_preprocessing.py

--- python/data_preprocessing.py
+++ python/data_preprocessing.py
@@ -4,26 +4,36 @@ import sys
 import logging
 import traceback
 
-"""
-Funciones para Municipios
-"""
-# Debido a que los código de municipio son locales al estado, es decir que se
-# repiten entre estados, se modifican para que sean únicos.
-# El código del estado se multiplica por 1000 y se le suma el código del
-# municipio para crear un código único.
-def make_municipio_id_unique(row):
-  estado_id = row['estado_id']
-  municipio_id = row['id']
-  old_id = row['id']
-  estado_id = estado_id * 1000
-  municipio_id = estado_id + municipio_id
-  row['id'] = municipio_id
-  row['old_id'] = old_id
+###
+# Funciones de utilidad
+###
+def make_municipio_id_unique(row: pd.Series) -> pd.Series:
+    """Convierte el id del municipio en id único
 
-  return row
+    Debido a que los código de municipio son locales al estado, es decir que se
+    repiten entre estados, es necesario modificarlos para que sean únicos.
+    El código del estado se multiplica por 1000 y se le suma el código del
+    municipio para crear un código único.
 
-# Remueve acentos y ñ de textos.
-def remove_accents(text):
+    Args:
+        row (pd.Series): Fila del municipio a modificar
+
+    Returns:
+        pd.Series: Fila con nuevo id y old_id
+    """
+
+    estado_id = row['estado_id']
+    municipio_id = row['id']
+    old_id = row['id']
+    estado_id = estado_id * 1000
+    municipio_id = estado_id + municipio_id
+    row['id'] = municipio_id
+    row['old_id'] = old_id
+    return row
+
+def remove_accents(text: str) -> str:
+    """Remueve acentos y ñ de textos"""
+
     # Normaliza a NFD (forma descompuesta de unicode)
     text = unicodedata.normalize('NFD', text)
     # Filtra los carácteres con acentos
@@ -32,11 +42,74 @@ def remove_accents(text):
     text = text.replace('ñ', 'n').replace('Ñ', 'N')
     return text
 
+###
+# Procesado de datos
+###
+def process_estados(df: pd.DataFrame) -> pd.DataFrame:
+    """Genera dataframe de estados"""
+
+    # Filtrar únicamente columnas de id y nombre
+    df_estados = df.filter(items=['estado_id', 'estado_nombre'])
+    # Renombrar columnas
+    df_estados = df_estados.rename(columns={ 'estado_id': 'id', 'estado_nombre': 'nombre' })
+    # Remueve filas duplicadas
+    df_estados = df_estados.drop_duplicates()
+    # Ordenar por id
+    df_estados.sort_values(by='id')
+    logging.debug(f'DF estados generado.')
+    return df_estados
+
+def process_municipios(df: pd.DataFrame) -> pd.DataFrame:
+    """Genera dataframe de municipios"""
+
+    # Toma solamente columnas necesarias
+    df_municipios = df.filter(items=['municipio_id', 'estado_id', 'municipio_nombre'])
+    # Renombra las columnas
+    df_municipios = df_municipios.rename(columns={ 'municipio_id': 'id', 'municipio_nombre': 'nombre'})
+    # Elimina filas duplicadas
+    df_municipios = df_municipios.drop_duplicates()
+    # Aplica función para convertir id a valores únicos
+    df_municipios = df_municipios.apply(make_municipio_id_unique, axis='columns')
+    # Ordena por id
+    df_municipios = df_municipios.sort_values(by='id')
+    logging.debug(f'DF municipios generado.')
+    return df_municipios
+
+def process_asentamientos(df: pd.DataFrame, df_municipios: pd.DataFrame) -> pd.DataFrame:
+    """Genera dataframe de asentamientos"""
+
+    # Filtra columnas necesarias
+    df_asentamientos = df.filter(items=['estado_id', 'municipio_id', 'nombre', 'tipo_asentamiento', 'ciudad', 'codigo_postal', 'tipo_zona'])
+    # Asocia el asentamiento con el id único del municipio
+    # Debido a que los id de los municipios cambiaron, es necesario hacer
+    # una instersección de los registros tomando como campos el id antiguo de los
+    # municipios y el id del estado.
+    df_asentamientos = df_asentamientos.merge(df_municipios, 'inner', left_on=['municipio_id', 'estado_id'], right_on=['old_id', 'estado_id'])
+    # Elimina columnas innecesarias y renombra otras
+    df_asentamientos = df_asentamientos.drop(columns=['estado_id', 'nombre_y', 'old_id', 'municipio_id']).rename(columns={ 'nombre_x': 'nombre', 'id': 'municipio_id' })
+    # Ordena las columnas y filas
+    df_asentamientos = df_asentamientos[['municipio_id', 'nombre', 'tipo_asentamiento', 'ciudad', 'codigo_postal', 'tipo_zona']].sort_values(by='municipio_id')
+    logging.debug(f'DF asentamientos generado.')
+    return df_asentamientos
+
+###
+# Generacion de enus
+###
+def get_enum(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Genera un dataframe con los cases y values para un enum de una columna"""
+
+    enum_cases = df.filter(items=[column]).drop_duplicates()[column]
+    df_enum = pd.DataFrame({
+        # Pasa a mayúsculas, reemplaza espacio por guión bajo, y remueve acentos.
+        'cases': enum_cases.apply(lambda case: remove_accents(case.upper().replace(' ', '_'))),
+        'values': enum_cases
+    })
+    return df_enum
+
 def main():
     logging.info('Iniciado data preprocessing.')
-    """
-    Carga de DF y renombramiento de columnas
-    """
+
+    ### Carga de DF y renombramiento de columnas ###
     # Toma el primer argumento como la ruta del archivo a cargar.
     file_path = sys.argv[1]
     # Tomal el segundo argumento como la ruta para exportar.
@@ -59,66 +132,24 @@ def main():
         'd_zona': 'tipo_zona',
     })
 
-    """
-    Estados
-    """
-    # Filtrar únicamente columnas de id y nombre
-    df_estados = df.filter(items=['estado_id', 'estado_nombre'])
-    # Renombrar columnas
-    df_estados = df_estados.rename(columns={ 'estado_id': 'id', 'estado_nombre': 'nombre' })
-    # Remueve filas duplicadas
-    df_estados = df_estados.drop_duplicates()
-    # Ordenar por id
-    df_estados.sort_values(by='id')
-    logging.debug(f'DF estados generado.')
+    ### Procesamiento de datos ###
+    # Estados
+    df_estados = process_estados(df)
 
-    """
-    Municipios
-    """
-    # Toma solamente columnas necesarias
-    df_municipios = df.filter(items=['municipio_id', 'estado_id', 'municipio_nombre'])
-    # Renombra las columnas
-    df_municipios = df_municipios.rename(columns={ 'municipio_id': 'id', 'municipio_nombre': 'nombre'})
-    # Elimina filas duplicadas
-    df_municipios = df_municipios.drop_duplicates()
-    # Aplica función para convertir id a valores únicos
-    df_municipios = df_municipios.apply(make_municipio_id_unique, axis='columns')
-    # Ordena por id
-    df_municipios = df_municipios.sort_values(by='id')
-    logging.debug(f'DF municipios generado.')
+    # Municipios
+    df_municipios = process_municipios(df)
 
-    """
-    Asentamientos
-    """
-    df_asentamientos = df.filter(items=['estado_id', 'municipio_id', 'nombre', 'tipo_asentamiento', 'ciudad', 'codigo_postal', 'tipo_zona'])
-    # Asocia el asentamiento con el id único del municipio
-    df_asentamientos = df_asentamientos.merge(df_municipios, 'inner', left_on=['municipio_id', 'estado_id'], right_on=['old_id', 'estado_id'])
-    # Elimina columnas innecesarias y renombra otras
-    df_asentamientos = df_asentamientos.drop(columns=['estado_id', 'nombre_y', 'old_id', 'municipio_id']).rename(columns={ 'nombre_x': 'nombre', 'id': 'municipio_id' })
-    # Ordena las columnas y filas
-    df_asentamientos = df_asentamientos[['municipio_id', 'nombre', 'tipo_asentamiento', 'ciudad', 'codigo_postal', 'tipo_zona']].sort_values(by='municipio_id')
-    logging.debug(f'DF asentamientos generado.')
+    # Asentamientos
+    df_asentamientos = process_asentamientos(df, df_municipios)
 
-    """
-    Enums
-    """
+    ### Enums ###
     # Tipo asentamiento
-    tipo_asentamiento_cases = df.filter(items=['tipo_asentamiento']).drop_duplicates()['tipo_asentamiento']
-    df_tipo_asentamiento = pd.DataFrame({
-        'cases': tipo_asentamiento_cases.apply(lambda case: remove_accents(case.upper().replace(' ', '_'))), # Pasa a mayúsculas y reemplaza espacio por guión bajo.
-        'values': tipo_asentamiento_cases
-    })
+    df_tipo_asentamiento = get_enum(df, 'tipo_asentamiento')
 
     # Tipo asentamiento
-    tipo_zona_cases = df.filter(items=['tipo_zona']).drop_duplicates()['tipo_zona']
-    df_tipo_zona = pd.DataFrame({
-        'cases': tipo_zona_cases.apply(lambda case: remove_accents(case.upper().replace(' ', '_'))), # Pasa a mayúsculas y reemplaza espacio por guión bajo.
-        'values': tipo_zona_cases
-    })
+    df_tipo_zona = get_enum(df, 'tipo_zona')
 
-    """
-    Exportacion a archivos CSV
-    """
+    ### Exportacion a archivos CSV ###
     logging.debug(f'Exportando DF a CSV.')
     df_estados.to_csv(f'{export_path}/estados.csv', index=False)
     df_municipios.to_csv(f'{export_path}/municipios.csv', index=False)
@@ -126,7 +157,6 @@ def main():
     df_tipo_asentamiento.to_csv(f'{export_path}/tipo_asentamiento_cases.csv', index=False, header=False)
     df_tipo_zona.to_csv(f'{export_path}/tipo_zona_cases.csv', index=False, header=False)
     logging.debug(f'CSV exportados.')
-
     logging.info('Finalizado data preprocessing.')
 
 if __name__ == "__main__":
