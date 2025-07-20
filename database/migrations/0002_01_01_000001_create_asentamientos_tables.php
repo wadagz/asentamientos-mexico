1741a93e725d4354a9ed5641428624e79b149711
wada <115588841+wadagz@users.noreply.github.com>
2025-07-19 21:47:26 -0600

    chore: removido importacion enums de migraciones.

--- database/migrations/0002_01_01_000001_create_asentamientos_tables.php
+++ database/migrations/0002_01_01_000001_create_asentamientos_tables.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Wadagz\AsentamientosMexico\Enums\Asentamiento\TipoAsentamientoEnum;
-use Wadagz\AsentamientosMexico\Enums\Asentamiento\TipoZonaEnum;
 
 return new class extends Migration
 {
